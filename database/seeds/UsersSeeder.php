d67dfacc207dc371e6a90820878f9123b558ecad
Javier M <javi.95.zgz@gmail.com>
2019-11-05 12:15:17 +0100

    Admin test user creation in seeder

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -20,5 +20,15 @@ class UsersSeeder extends Seeder
                 $user->role()->associate($role)->save();
                 $user->teams()->attach($team);
             });
+        $admin = new App\User;
+        $admin->email = 'admin@admin.com';
+        $admin->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
+
+        // Add a random role and team
+        $role = App\Role::inRandomOrder()->first();
+        $team = App\Team::inRandomOrder()->first();
+        $admin->role()->associate($role)->save();
+        $admin->teams()->attach($team);
+        $admin->save();
     }
 }
