a20b757e1d44de597eac4f21106edb80878d1f13
Javier M <javi.95.zgz@gmail.com>
2021-01-18 14:07:15 +0100

    Minor changes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        // $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', 'zeal-api.herokuapp.com');
         return $response;
     }
 }
