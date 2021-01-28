6cb1bb6f00901c42eae1e91ba7693f7b1adfe89b
Javier M <javi.95.zgz@gmail.com>
2021-01-28 20:55:08 +0100

    Missing model dependency changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -105,7 +105,7 @@ class User extends Authenticatable implements JWTSubject
 	 */
 	public function tasks()
 	{
-		return $this->hasMany('App\Task');
+		return $this->hasMany('App\Models\Task');
 	}
 
     /**
@@ -113,7 +113,7 @@ class User extends Authenticatable implements JWTSubject
      */
 	public function task_comments()
     {
-        return $this->belongsToMany('App\TaskComment');
+        return $this->belongsToMany('App\Models\TaskComment');
     }
 
 	/**
